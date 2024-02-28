51fbb8a6cc9f45baff81d886551388bb175c13e5
Jacobs <jakub.banach72@gmail.com>
2024-02-28 19:55:45 +0100

    Adding links for reactive tests development

---
+++ src/test/java/com/example/Arifutera/github/GitHubRepositoriesReactiveFlowIT.java
@@ -0,0 +1,11 @@
+package com.example.Arifutera.github;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GitHubRepositoriesReactiveFlowIT {
+    // TODO: https://spring.io/guides/gs/reactive-rest-service
+    //  https://docs.spring.io/spring-framework/reference/integration/rest-clients.html#rest-restclient
+
+
+}
